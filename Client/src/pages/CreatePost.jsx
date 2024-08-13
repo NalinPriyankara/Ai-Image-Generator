a07187fac006945e3c47a6b37ef0e26b8610a4db
NalinPriyankara <nalinpriyankara033@gmail.com>
2024-08-13 18:30:04 +0530

    update

--- Client/src/pages/CreatePost.jsx
+++ Client/src/pages/CreatePost.jsx
@@ -16,7 +16,7 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading]= useState(false);
 
-    const generateImage = async () => {
+    const generatingImage = async () => {
         if(form.prompt) {
             try {
                 setGeneratingImg(true);
@@ -109,7 +109,7 @@ const CreatePost = () => {
             <div className='mt-5 flex gap-5'>
                 <button
                     type='button'
-                    onClick={generateImage}
+                    onClick={generatingImage}
                     className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
                 >
                     {generatingImg ? 'Generating...' : 'Generate'}
