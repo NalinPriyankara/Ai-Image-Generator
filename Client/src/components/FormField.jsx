9905d3a04fed800758fd5836af579845992fba26
NalinPriyankara <nalinpriyankara033@gmail.com>
2024-08-17 16:47:39 +0530

    backup

--- Client/src/components/FormField.jsx
+++ Client/src/components/FormField.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { preview } from '../assets'
 
-const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
+const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
     <div>
       <div className='flex items-center gap-2 mb-2'>
         <label
           htmlFor={name}
-          className='block text-sm font-medium text-gray-900'
+          className='block text-sm font-medium tex-gray-900'
         >
-          {labelName}
+          {LabelName}
         </label>
         {isSurpriseMe && (
           <button
             type='button'
             onClick={handleSurpriseMe}
-            className='font-bold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+            className='font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black'
           >
             Surprise Me
           </button>
@@ -28,7 +29,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
         value={value}
         onChange={handleChange}
         required
-        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block  w-full p-3'
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
       />
     </div>
   )
--- Client/src/pages/CreatePost.jsx
+++ Client/src/pages/CreatePost.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
-import { getRandomPrompt } from '../utils';
+import { getRandomPrompt } from '../utils/index.js';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
-    const navigate = useNavigate();
+    const naviagte = useNavigate();
     const [form, setForm] = useState({
         name: '',
         prompt: '',
         photo: '',
     });
     const [generatingImg, setGeneratingImg] = useState(false);
-    const [loading, setLoading]= useState(false);
+    const [loading, setLoading] = useState(false);
 
     const generateImage = async () => {
         if(form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                const response = await fetch('https://localhost:8080/api/v1/dalle', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -30,14 +29,14 @@ const CreatePost = () => {
 
                 const data = await response.json();
 
-                setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
+                setForm({ ...form, photo: `data:image/jpeg:base64,${data.photo}`})
             } catch (error) {
                 alert(error);
             } finally {
                 setGeneratingImg(false);
             }
         } else {
-            alert('Please enter a prompt');
+            alert('Please enter a prompt')
         }
     }
 
@@ -58,48 +57,47 @@ const CreatePost = () => {
     <section className='max-w-7xl mx-auto'>
         <div>
             <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
-            <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
-                Create imaginative and visually stunning images through DALL-E AI and share them with the community.
-            </p>
+            <p className='mt-2 text-[#666e75] text-[16px] max-w [500px]'>Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
         </div>
+
         <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
             <div className='flex flex-col gap-5'>
-                <FormField 
-                    labelName='Your name'
-                    type='text'
-                    name='name'
-                    placeholder='John Doe'
+                <FormField
+                    LabelName="Your name"
+                    type="text"
+                    name="name"
+                    placeholder="Jhon Doe"
                     value={form.name}
                     handleChange={handleChange}
                 />
-                <FormField 
-                    labelName='Prompt'
-                    type='text'
-                    name='prompt'
-                    placeholder='A comic book cover of a superhero wearing headphones'
+                <FormField
+                    LabelName="Prompt"
+                    type="text"
+                    name="prompt"
+                    placeholder="A comic book cover of a superhero wearing headphones"
                     value={form.prompt}
                     handleChange={handleChange}
                     isSurpriseMe
-                    hadleSurpriseMe={handleSurpriseMe}
+                    handleSurpriseMe={handleSurpriseMe}
                 />
 
-                <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+                <div className='relative bg-gray-50 border border-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
                     {form.photo ? (
                         <img
                             src={form.photo}
-                            alt={form.prompt}
+                            alt={form.photo}
                             className='w-full h-full object-contain'
-                        />
+                         />
                     ) : (
-                        <img
-                            src={preview}
-                            alt='preview'
-                            className='w-9/12 h-9/12 object-contain opacity-40'
-                        />
+                    <img
+                        src={preview}
+                        alt='preview'
+                        className='w-9/12 h-9/12 object-contain opacity-40'
+                    />
                     )}
 
                     {generatingImg && (
-                        <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                        <div className='absolute inset-0 z-0 flex justify-center items-center bg[rgba(0,0,0,0.5)] rounded-lg'>
                             <Loader />
                         </div>
                     )}
@@ -116,13 +114,11 @@ const CreatePost = () => {
                 </button>
             </div>
 
-            <div className='mt-16'>
-                <p className='mt-2 text-[#666e75] text-[14px]'>
-                    Once you have created the image you want, you can share it with others in the community.
-                </p>
+            <div className='mt-10'>
+                <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community</p>
                 <button
                     type='submit'
-                    className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+                    className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text.center'
                 >
                     {loading ? 'Sharing...' : 'Share with the community'}
                 </button>
